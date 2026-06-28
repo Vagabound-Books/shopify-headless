3a2d7a612d5e8abce9f83f71e2ea3c631c86a5b3
dluman <dluman@allegheny.edu>
2026-06-28 14:08:38 -0400

    Adding adapter to deploy on DigitalOcean

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact()],
   output: 'server',
-  adapter: undefined,
+  adapter: node({ mode: 'standalone' }),
 });
